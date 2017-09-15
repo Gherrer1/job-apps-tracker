b16ce78a2950adb87c6858546f048f5b9c69fcf1
Gerardo Herrera <gherrer1@binghamton.edu>
2017-09-14 21:56:41 -0400

    cleaned up googleApiApp.js

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -74,142 +74,141 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
-(function() {
 
-	
-	var CLIENT_ID = '643118581198-1ahtvd2u2o98l2hur59mrctu60km0gb7.apps.googleusercontent.com';
-	// Array of API discovery doc URLs for APIs used by the quickstart - I guess this adds namespaces (gmail, sheets) to the gapi.client object
-	// because we were allowed to access them and make requests with them before we added the necessary scopes, we just received 403 responses
-	var DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4', 'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest', 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
-	// Authorization scopes required by the API; multiple scopes can be included, separated by spaces. - tells googles servers that your app/clientID
-	// can make certain requests
-	var SCOPES = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/drive";
+var CLIENT_ID = '643118581198-1ahtvd2u2o98l2hur59mrctu60km0gb7.apps.googleusercontent.com';
+// Array of API discovery doc URLs for APIs used by the quickstart - I guess this adds namespaces (gmail, sheets) to the gapi.client object
+// because we were allowed to access them and make requests with them before we added the necessary scopes, we just received 403 responses
+var DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4', 'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest', 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
+// Authorization scopes required by the API; multiple scopes can be included, separated by spaces. - tells googles servers that your app/clientID
+// can make certain requests
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/drive";
 
-	var authorizeButton = document.getElementById('authorize-button');
-	var signoutButton = document.getElementById('signout-button');
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
 
-	/**
-	 * On load, called to load the auth2 library and API client library.
-	 */
-	function handleClientLoad() { // yeah, this is called by onload attribute of script tag that loads google's js. this is called after google's js executes
-		gapi.load('client:auth2', initClient);
-	}
+/**
+ * On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() { // yeah, this is called by onload attribute of script tag that loads google's js. this is called after google's js executes
+	gapi.load('client:auth2', initClient);
+}
 
-	/**
-	 * Initializes the API client library and sets up sign-in state listeners.
-	 */
-	function initClient() {
-		gapi.client.init({
-			discoveryDocs: DISCOVERY_DOCS,
-			clientId: CLIENT_ID,
-			scope: SCOPES
-		}).then(function() { // after we've initialized the client library
-			// listen for sign-in state changes
-			gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+/**
+ * Initializes the API client library and sets up sign-in state listeners.
+ */
+function initClient() {
+	gapi.client.init({
+		discoveryDocs: DISCOVERY_DOCS,
+		clientId: CLIENT_ID,
+		scope: SCOPES
+	}).then(function() { // after we've initialized the client library
+		// listen for sign-in state changes
+		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-			// handle the initial sign-in state.
-			updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-			authorizeButton.onclick = handleAuthClick;
-			signoutButton.onclick = handleSignoutClick;
+		// handle the initial sign-in state.
+		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+		authorizeButton.onclick = handleAuthClick;
+		signoutButton.onclick = handleSignoutClick;
+	});
+}
+
+/* *
+ * Called when the signed in status changes, tto update the UI appropriately. After a sign=in, the API is called
+ */
+function updateSigninStatus(isSignedIn) {
+	if(isSignedIn) {
+		authorizeButton.style.display = 'none';
+		signoutButton.style.display = 'block';
+		
+		/* Essentially where all the magic happens. Once app acknowledges that user is signed in, the app can start running. */
+		appendPre('Logged in, looking for your organizer sheet...');
+
+		var JOB_APPS_ORGANIZER_SHEET_NAME = 'fake31';
+		var _trimmedEmailData;
+
+		__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].initWithSheetNamed(JOB_APPS_ORGANIZER_SHEET_NAME)
+		.then(id => appendPre('Sheet ID: ' + id))
+		.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].readLastScanMetaData)
+		.then(metaData => { metaData ? appendPre('Date: ' + metaData.date + '\nRow: ' + metaData.row) : appendPre('Brand new sheet, no meta data yet')})
+		.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].getMetaData).then(metaData => metaData.date)
+		.then(__WEBPACK_IMPORTED_MODULE_1__Mail__["a" /* default */].loadEmailsAfter)
+		.then(messages => {
+			messages.apps_sent = messages.apps_sent.map( trimEmailJsonFat );
+			messages.apps_rejected = messages.apps_rejected.map ( trimEmailJsonFat );
+			messages.apps_interested = messages.apps_interested.map( trimEmailJsonFat );
+			return messages;
+		})
+		.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].recordAppStatusesFromEmails)
+		.then(res => console.log(res))
+		.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].writeLastScanMetaData)
+		.then(result => { appendPre('Updated meta data'); console.log(result); })
+		.then(appendPre.bind(null, 'Done!'))
+		.catch(function(errorMsg) {
+			//
+			console.log(errorMsg);
 		});
+	} else {
+		authorizeButton.style.display = 'block';
+		signoutButton.style.display = 'none';
 	}
+}
 
-	/* *
-	 * Called when the signed in status changes, tto update the UI appropriately. After a sign=in, the API is called
-	 */
-	function updateSigninStatus(isSignedIn) {
-		if(isSignedIn) {
-			authorizeButton.style.display = 'none';
-			signoutButton.style.display = 'block';
-			
-			/* Essentially where all the magic happens. Once app acknowledges that user is signed in, the app can start running. */
-			appendPre('Logged in, looking for your organizer sheet...');
+/**
+ * Sign in the user upon button click
+ */
+function handleAuthClick(event) {
 
-			var JOB_APPS_ORGANIZER_SHEET_NAME = 'fake31';
-			var _trimmedEmailData;
+	gapi.auth2.getAuthInstance().signIn();
+}
 
-			__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].initWithSheetNamed(JOB_APPS_ORGANIZER_SHEET_NAME)
-			.then(id => appendPre('Sheet ID: ' + id))
-			.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].readLastScanMetaData)
-			.then(metaData => { metaData ? appendPre('Date: ' + metaData.date + '\nRow: ' + metaData.row) : appendPre('Brand new sheet, no meta data yet')})
-			.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].getMetaData).then(metaData => metaData.date)
-			.then(__WEBPACK_IMPORTED_MODULE_1__Mail__["a" /* default */].loadEmailsAfter)
-			.then(messages => {
-				messages.apps_sent = messages.apps_sent.map( trimEmailJsonFat );
-				messages.apps_rejected = messages.apps_rejected.map ( trimEmailJsonFat );
-				messages.apps_interested = messages.apps_interested.map( trimEmailJsonFat );
-				return messages;
-			})
-			.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].recordAppStatusesFromEmails)
-			.then(res => console.log(res))
-			.then(__WEBPACK_IMPORTED_MODULE_0__Sheets__["a" /* default */].writeLastScanMetaData)
-			.then(result => { appendPre('Updated meta data'); console.log(result); })
-			.then(appendPre.bind(null, 'Done!'))
-			.catch(function(errorMsg) {
-				//
-				console.log(errorMsg);
-			});
-		} else {
-			authorizeButton.style.display = 'block';
-			signoutButton.style.display = 'none';
-		}
+/**
+ * Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+	gapi.auth2.getAuthInstance().signOut();
+	clearPre();
+}
+
+/**
+ * Append a pre element to the body containing the given message as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in the pre element.
+ */
+function appendPre(message) {
+	var pre = document.getElementById('content');
+	var textContent = document.createTextNode(message + '\n');
+	pre.appendChild(textContent);
+}
+
+/* removes all the text from the pre element */
+function clearPre() {
+	var pre = document.getElementById('content');
+	pre.innerHTML = '';
+}
+
+/**
+ * Email JSON comes with alot of stuff we wont use. Only take what we need
+ * @param {Email} email The emailData we'll be trimming
+ * @return {LighterEmail} A new object with just the fields we'll be using
+ */
+function trimEmailJsonFat(email) {
+	var headers = email.result.payload.headers;
+	var lighterEmail = { date: null, from: null }; // Subject to expand as app grows in complexity
+	for(var i = 0; i < headers.length; i++) {
+		if(lighterEmail.data && lighterEmail.from)
+			break;
+		if(headers[i].name === 'Date')
+			lighterEmail.date = headers[i].value;
+		if(headers[i].name === 'From')
+			lighterEmail.from = headers[i].value
 	}
+	// lighterEmail.labelName = email.result.labelName;
+	return lighterEmail;
+}
 
-	/**
-	 * Sign in the user upon button click
-	 */
-	function handleAuthClick(event) {
-
-		gapi.auth2.getAuthInstance().signIn();
-	}
-
-	/**
-	 * Sign out the user upon button click.
-	 */
-	function handleSignoutClick(event) {
-		gapi.auth2.getAuthInstance().signOut();
-		clearPre();
-	}
-
-	/**
-	 * Append a pre element to the body containing the given message as its text node. Used to display the results of the API call.
-	 *
-	 * @param {string} message Text to be placed in the pre element.
-	 */
-	function appendPre(message) {
-		var pre = document.getElementById('content');
-		var textContent = document.createTextNode(message + '\n');
-		pre.appendChild(textContent);
-	}
-
-	/* removes all the text from the pre element */
-	function clearPre() {
-		var pre = document.getElementById('content');
-		pre.innerHTML = '';
-	}
-
-	/**
-	 * Email JSON comes with alot of stuff we wont use. Only take what we need
-	 * @param {Email} email The emailData we'll be trimming
-	 * @return {LighterEmail} A new object with just the fields we'll be using
-	 */
-	function trimEmailJsonFat(email) {
-		var headers = email.result.payload.headers;
-		var lighterEmail = { date: null, from: null }; // Subject to expand as app grows in complexity
-		for(var i = 0; i < headers.length; i++) {
-			if(lighterEmail.data && lighterEmail.from)
-				break;
-			if(headers[i].name === 'Date')
-				lighterEmail.date = headers[i].value;
-			if(headers[i].name === 'From')
-				lighterEmail.from = headers[i].value
-		}
-		// lighterEmail.labelName = email.result.labelName;
-		return lighterEmail;
-	}
-
-	window.handleClientLoad = handleClientLoad;
-})();
+// Expose handleClientLoad to the window/global scope so that 
+// google's script can run this onload
+window.handleClientLoad = handleClientLoad;
 
 /* Note:
 	This app sort of has a blind spot. If an email scan is conducted on 9/9/2017,
