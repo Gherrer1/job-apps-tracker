9dda6f46be75b5d9bb93cf56597d07087c9591f9
Gerardo Herrera <gherrer1@binghamton.edu>
2017-09-14 21:48:43 -0400

    cleaned up Mail.js

--- public/js/Mail.js
+++ public/js/Mail.js
@@ -1,86 +1,83 @@
 import Util from './Util';
 
-var Mail = (function() {
-	var _emails = {
-		_apps_sent: null,
-		_apps_rejected: null,
-		_apps_interested: null
+var _emails = {
+	_apps_sent: null,
+	_apps_rejected: null,
+	_apps_interested: null
+};
+
+function loadEmailsAfter(date) {
+	return scanAll(date);
+}
+
+function scanAll(date) {
+	return new Promise(function(resolve, reject) {
+		var promises = [
+			getMessagesLabeled('apps-sent', date),
+			getMessagesLabeled('apps-rejected', date),
+			getMessagesLabeled('apps-interested', date)
+		]
+		Promise.all(promises)
+		.then(dataSet => {
+			_emails._apps_sent = dataSet[0]; 		// apps-sent
+			_emails._apps_rejected = dataSet[1]; 	// apps-rejected
+			_emails._apps_interested = dataSet[2]; 	// apps-interested
+		})
+		.then(() => resolve( getEmails() ) );
+	});
+}
+
+function getEmails() {
+	return {
+		apps_sent: _emails._apps_sent, apps_rejected: _emails._apps_rejected, apps_interested: _emails._apps_interested
 	};
-	
-	function loadEmailsAfter(date) {
-		return scanAll(date);
-	}
+}
 
-	function scanAll(date) {
-		return new Promise(function(resolve, reject) {
-			var promises = [
-				getMessagesLabeled('apps-sent', date),
-				getMessagesLabeled('apps-rejected', date),
-				getMessagesLabeled('apps-interested', date)
-			]
-			Promise.all(promises)
-			.then(dataSet => {
-				_emails._apps_sent = dataSet[0]; 		// apps-sent
-				_emails._apps_rejected = dataSet[1]; 	// apps-rejected
-				_emails._apps_interested = dataSet[2]; 	// apps-interested
-			})
-			.then(() => resolve( getEmails() ) );
+function getMessagesLabeled(label, date) {
+	return new Promise(function(resolve, reject) {
+		var label_based_query = 'label:'+label;
+		if(date) {
+			label_based_query += ' after:' + Util.dateFormatter(date);
+		}
+		var apiParams = { userId: 'me', q: label_based_query, maxResults: 1000 };
+		gapi.client.gmail.users.messages.list(apiParams)
+		.then(response => { 
+			var messageData = response.result.messages || [];
+			return getMessagesByIds( messageData.map(msg => msg.id) ); 
+		})
+		.then(messages => {
+			resolve(messages)
 		});
-	}
+	});
+}
 
-	function getEmails() {
-		return {
-			apps_sent: _emails._apps_sent, apps_rejected: _emails._apps_rejected, apps_interested: _emails._apps_interested
-		};
-	}
+// Study this
+function getMessagesByIds(ids) {
+	return new Promise(function(resolve, reject) {
+		if(ids.length === 0)
+			return resolve([]);
+		var ajaxCallsRemaining = ids.length;
+		var max = ajaxCallsRemaining;
+		var returnedData = [];
 
-	function getMessagesLabeled(label, date) {
-		return new Promise(function(resolve, reject) {
-			var label_based_query = 'label:'+label;
-			if(date) {
-				label_based_query += ' after:' + Util.dateFormatter(date);
-			}
-			var apiParams = { userId: 'me', q: label_based_query, maxResults: 1000 };
-			gapi.client.gmail.users.messages.list(apiParams)
-			.then(response => { 
-				var messageData = response.result.messages || [];
-				return getMessagesByIds( messageData.map(msg => msg.id) ); 
-			})
-			.then(messages => {
-				resolve(messages)
-			});
-		});
-	}
+		for(var i = 0; i < ids.length; i++) {
+			gapi.client.gmail.users.messages.get({ userId: 'me', id: ids[i] })
+				.then(function(response) {
+					returnedData.push(response);
+					// console.log(response);
+					--ajaxCallsRemaining;
+					if(ajaxCallsRemaining <= 0) {
+						return resolve(returnedData);
+					}
+				});
+		}
+	});
+}
 
-	// Study this
-	function getMessagesByIds(ids) {
-		return new Promise(function(resolve, reject) {
-			if(ids.length === 0)
-				return resolve([]);
-			var ajaxCallsRemaining = ids.length;
-			var max = ajaxCallsRemaining;
-			var returnedData = [];
+var MailAPI = {
+	loadEmailsAfter: loadEmailsAfter,
+	getMessagesByIds: getMessagesByIds,
+	getEmails: getEmails
+};
 
-			for(var i = 0; i < ids.length; i++) {
-				gapi.client.gmail.users.messages.get({ userId: 'me', id: ids[i] })
-					.then(function(response) {
-						returnedData.push(response);
-						// console.log(response);
-						--ajaxCallsRemaining;
-						if(ajaxCallsRemaining <= 0) {
-							return resolve(returnedData);
-						}
-					});
-			}
-		});
-	}
-
-	var publicAPI = {
-		loadEmailsAfter: loadEmailsAfter,
-		getMessagesByIds: getMessagesByIds,
-		getEmails: getEmails
-	};
-	return publicAPI;
-})();
-
-export default Mail;
+export default MailAPI;
